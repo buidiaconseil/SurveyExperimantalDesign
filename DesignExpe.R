2f66cdd92e4111c9a5cdda742f599dc8c3c76e20
SEBASTIEN DIAZ <sebastien-externe.diaz@enedis.fr>
2018-07-12 15:38:19 +0200

    omi more

---
+++ DesignExpe.R
@@ -0,0 +1,16 @@
+library('AlgDesign')
+cand.list <- expand.grid(ChatbotNeed = c("yes", "no"), MachinelearningNeed = c("yes", "no"),
+  AdvancedMachineLearningNeed = c("yes", "no")
+  ,Amountofdata = c("huge", "less")
+  ,Budget = c(">100k", "<100k")
+  ,HappyMLsolution = c("yes", "no")
+  ,ExtendInvestment = c("yes", "no")
+  ,Cloudneed = c("yes", "no")
+  )
+
+###same as SPSS orthogonal design 'seed'. Can put any number. Does not matter.
+set.seed(69)
+
+###Generate 16 alternatives in an optimal orthogonal design
+simu<-optFederov( ~ ., data = cand.list, nTrials = 10)
+  
--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # SurveyExperimantalDesign
 Experimental Design of Surveys. Searching an optimized way to minimize questions on a survey.
 
+# List of Subject
+
+Chatbot need: yes no
+Machine learning : y/n
+Advanced Machine Learning: yn
+Amount of data: Huge or Less
+Budget : >100 k
+Happy on ML solution : yn
+Extend Investment: yn
+Cloud need : yn
+
+
+
 # License
 See LICENSE file
 
